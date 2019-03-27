03ba4e0378117c421392182713db47391b3a4dc0
Cristopher Robles <crisrc012@hotmail.com>
2019-03-26 18:54:28 -0600

    Preparación para BD

---
+++ ClubCampestre/ClubCampestre_BLL/BD/Cls_BD_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubCampestre_BLL.BD
+{
+    public class Cls_BD_BLL
+    {
+    }
+}
---
+++ ClubCampestre/ClubCampestre_DAL/BD/Cls_BD_DAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubCampestre_DAL.BD
+{
+    public class Cls_BD_DAL
+    {
+    }
+}
