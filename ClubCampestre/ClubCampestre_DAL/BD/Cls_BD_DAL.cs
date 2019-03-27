286ca04bb79be4a076da6f6f1f7f3a696b43e9cf
Cristopher Robles <crisrc012@hotmail.com>
2019-03-26 19:02:21 -0600

    Clase DAL para manejo de datos con la base de datos

--- ClubCampestre/ClubCampestre_DAL/BD/Cls_BD_DAL.cs
+++ ClubCampestre/ClubCampestre_DAL/BD/Cls_BD_DAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,39 @@ namespace ClubCampestre_DAL.BD
 {
     public class Cls_BD_DAL
     {
+        #region Variables
+        private string _smsj_error, _scadena_conexion;
+
+        public string Smsj_error
+        {
+            get
+            {
+                return _smsj_error;
+            }
+
+            set
+            {
+                _smsj_error = value;
+            }
+        }
+
+        public string Scadena_conexion
+        {
+            get
+            {
+                return _scadena_conexion;
+            }
+
+            set
+            {
+                _scadena_conexion = value;
+            }
+        }
+        #endregion
+        #region Objetos de base de datos
+        public SqlCommand Obj_sql_cmd;
+        public SqlDataAdapter Obj_sql_adap;
+        public SqlConnection Obj_sql_cnx;
+        #endregion
     }
 }
