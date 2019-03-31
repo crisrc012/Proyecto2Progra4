626c3a4466c31c779b363a69ac205bc6cfde85a7
Cristopher Robles <crisrc012@hotmail.com>
2019-03-31 16:35:42 -0600

    Avance en CRUD

--- ClubCampestre/ClubCampestre_BLL/CatalogosMantenimientos/Cls_Clientes_BLL.cs
+++ ClubCampestre/ClubCampestre_BLL/CatalogosMantenimientos/Cls_Clientes_BLL.cs
@@ -1,6 +1,7 @@
 ﻿using ClubCampestre_BLL.BD;
 using ClubCampestre_DAL.BD;
 using ClubCampestre_DAL.CatalogosMantenimientos;
+using System;
 
 namespace ClubCampestre_BLL.CatalogosMantenimientos
 {
@@ -31,6 +32,10 @@ namespace ClubCampestre_BLL.CatalogosMantenimientos
         {
             Obj_BD_DAL = new Cls_BD_DAL();
             Obj_BD_DAL.sNombre_SP = "[dbo].[sp_search_TB_Clientes]";
+            // Se cargan valores a buscar
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdCliente", Obj_Clientes_DAL.SIdCliente);
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdTipoCliente", Obj_Clientes_DAL.BIdTipoCliente);
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdPersona", Obj_Clientes_DAL.SIdPersona);
             Obj_Clientes_DAL.DS.Tables.Add(Obj_BD_BLL.ExecuteDataAdapter(ref Obj_BD_DAL).Copy());
             if (Obj_BD_DAL.sMsj_error == string.Empty)
             {
@@ -45,17 +50,59 @@ namespace ClubCampestre_BLL.CatalogosMantenimientos
 
         public void Insertar(ref Cls_Clientes_DAL Obj_Clientes_DAL)
         {
-
+            Obj_BD_DAL = new Cls_BD_DAL();
+            Obj_BD_DAL.sNombre_SP = "[dbo].[sp_insert_TB_Clientes]";
+            // Se cargan valores a insertar
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdTipoCliente", Obj_Clientes_DAL.BIdTipoCliente);
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdPersona", Obj_Clientes_DAL.SIdPersona);
+            Obj_Clientes_DAL.SIdCliente = Convert.ToInt16(Obj_BD_BLL.ExecuteScalar(ref Obj_BD_DAL));
+            if (Obj_BD_DAL.sMsj_error == string.Empty)
+            {
+                Obj_Clientes_DAL.SMsjError = string.Empty;
+            }
+            else
+            {
+                Obj_Clientes_DAL.SMsjError = Obj_BD_DAL.sMsj_error;
+                Obj_Clientes_DAL.DS = null;
+            }
         }
 
         public void Actualizar(ref Cls_Clientes_DAL Obj_Clientes_DAL)
         {
-
+            Obj_BD_DAL = new Cls_BD_DAL();
+            Obj_BD_DAL.sNombre_SP = "[dbo].[sp_update_TB_Clientes]";
+            // Se cargan valores a actualizar
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdCliente", Obj_Clientes_DAL.SIdCliente);
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdTipoCliente", Obj_Clientes_DAL.BIdTipoCliente);
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdPersona", Obj_Clientes_DAL.SIdPersona);
+            Obj_BD_BLL.ExecuteNonQuery(ref Obj_BD_DAL);
+            if (Obj_BD_DAL.sMsj_error == string.Empty)
+            {
+                Obj_Clientes_DAL.SMsjError = string.Empty;
+            }
+            else
+            {
+                Obj_Clientes_DAL.SMsjError = Obj_BD_DAL.sMsj_error;
+                Obj_Clientes_DAL.DS = null;
+            }
         }
 
         public void Eliminar(ref Cls_Clientes_DAL Obj_Clientes_DAL)
         {
-
+            Obj_BD_DAL = new Cls_BD_DAL();
+            Obj_BD_DAL.sNombre_SP = "[dbo].[sp_delete_TB_Clientes]";
+            // Se cargan valores a eliminar
+            Obj_BD_DAL.Obj_dtparam.Rows.Add("@IdCliente", Obj_Clientes_DAL.SIdCliente);
+            Obj_BD_BLL.ExecuteNonQuery(ref Obj_BD_DAL);
+            if (Obj_BD_DAL.sMsj_error == string.Empty)
+            {
+                Obj_Clientes_DAL.SMsjError = string.Empty;
+            }
+            else
+            {
+                Obj_Clientes_DAL.SMsjError = Obj_BD_DAL.sMsj_error;
+                Obj_Clientes_DAL.DS = null;
+            }
         }
     }
 }
--- ClubCampestre/WCF/Interfaces/ICatalogosMantenimientos.cs
+++ ClubCampestre/WCF/Interfaces/ICatalogosMantenimientos.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.ServiceModel;
 
 namespace WCF.Interfaces
@@ -6,9 +7,46 @@ namespace WCF.Interfaces
     [ServiceContract]
     public interface ICatalogosMantenimientos
     {
+        #region Beneficiarios
+        [OperationContract]
+        DataTable listarBeneficiarios();
+        [OperationContract]
+        DataTable filtrarBeneficiarios(short sIdBeneficiario, short sIdCliente, string sIdPersona, char cIdEstado);
+        #endregion
+        #region Clientes
+        [OperationContract]
+        DataTable listarClientes();
+        [OperationContract]
+        DataTable filtrarClientes(short sIdCliente, byte bIdTipoCliente, string sIdPersona);
+        #endregion
+        #region Correos
+        [OperationContract]
+        DataTable listarCorreos();
+        [OperationContract]
+        DataTable filtrarCorreos(short sIdCorreo, string sIdPersona, string sCorreo);
+        #endregion
         #region Estado
         [OperationContract]
         DataTable listarEstado();
+        [OperationContract]
+        DataTable filtrarEstado(char cIdEstado, string sEstado);
+        [OperationContract]
+        char insertarEstado(char cIdEstado, string sEstado);
+        [OperationContract]
+        bool actualizarEstado(char cIdEstado, string sEstado);
+        [OperationContract]
+        bool eliminarEstado(char cIdEstado);
+        #endregion
+        #region Facturacion
+        DataTable listarFacturacion();
+        [OperationContract]
+        DataTable filtrarFacturacion(short sIdCliente, string sDescripcion, DateTime DFecha, float fMontototal);
+        #endregion
+        #region FacturaDetalle
+        [OperationContract]
+        DataTable listarFacturaDetalle();
+        [OperationContract]
+        DataTable filtrarFacturaDetalle(int iIdFacturaDetalle, int iIdFactura, string sDetalle, float fcosto, byte bIdTipoServicio, int iIdMembresia, int icantidad, float ftotal);
         #endregion
     }
 }
