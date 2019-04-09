14ab3c34f57d0472b9e662a999b8be15458ae33f
Cristopher Robles <crisrc012@hotmail.com>
2019-04-09 17:37:48 -0600

    Cuenta de servicio de SQL

---
+++ Base de datos/Login.sql
@@ -0,0 +1,12 @@
+USE [master]
+GO
+CREATE LOGIN [servicio] WITH PASSWORD=N'Servicio123', DEFAULT_DATABASE=[ClubCampestre], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+USE [ClubCampestre]
+GO
+CREATE USER [servicio] FOR LOGIN [servicio]
+GO
+USE [ClubCampestre]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [servicio]
+GO
