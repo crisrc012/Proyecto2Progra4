e331531bde8eaec5ef180acf9e01c4533c479430
Cristopher Robles <crisrc012@hotmail.com>
2019-04-09 18:28:21 -0600

    SVC generico cómo el profe la presentó

--- ClubCampestre/ClubCampestre_BLL/BD/Cls_BD_BLL.cs
+++ ClubCampestre/ClubCampestre_BLL/BD/Cls_BD_BLL.cs
@@ -40,7 +40,7 @@ namespace ClubCampestre_BLL.BD
                         foreach (DataRow Row in dtParams.Rows)
                         {
                             Obj_BD_DAL.Obj_sql_adap.SelectCommand.Parameters.Add(Row[0].ToString()
-                                , volverDatoSQL(Row[0].GetType())).Value = Row[1];
+                                , volverDatoSQL(Row[1].GetType())).Value = Row[1];
                         }
                     }
                 }
@@ -62,6 +62,50 @@ namespace ClubCampestre_BLL.BD
             catch (SqlException e)
             {
                 sMsj_error = e.ToString().Trim();
+                return null;
+            }
+            finally
+            {
+                if (Obj_BD_DAL.Obj_sql_cnx != null)
+                {
+                    if (Obj_BD_DAL.Obj_sql_cnx.State == ConnectionState.Open)
+                    {
+                        Obj_BD_DAL.Obj_sql_cnx.Close();
+                    }
+                    Obj_BD_DAL.Obj_sql_cnx.Dispose();
+                }
+            }
+        }
+        public DataTable ExecuteDataAdapter(string sNombreSP, string sNombreParametro, SqlDbType DbType, string sValorParametro, ref string sMsjError)
+        {
+            Cls_BD_DAL Obj_BD_DAL = new Cls_BD_DAL();
+            try
+            {
+                // Se crea el objeto de conexión
+                Obj_BD_DAL.Obj_sql_cnx = new SqlConnection(ConfigurationManager.ConnectionStrings["Sql_aut_clubcampestre"].ToString().Trim());
+                // Se inicializa el DataAdapter con el SP y la conexión abierta
+                Obj_BD_DAL.Obj_sql_adap = new SqlDataAdapter(sNombreSP, Obj_BD_DAL.Obj_sql_cnx);
+                // Asignar parámetros
+                Obj_BD_DAL.Obj_sql_adap.SelectCommand.Parameters.Add(sNombreParametro
+                    , DbType).Value = sValorParametro;
+                // Se especifica el tipo de comando de SP
+                Obj_BD_DAL.Obj_sql_adap.SelectCommand.CommandType = CommandType.StoredProcedure;
+                // Si la conexión está cerrada
+                if (Obj_BD_DAL.Obj_sql_cnx.State == ConnectionState.Closed)
+                {
+                    // Se abre la cadena de conexión
+                    Obj_BD_DAL.Obj_sql_cnx.Open();
+                }
+                // Se inicializa el DataSet
+                DataSet DS = new DataSet();
+                // Se carga el DataSet
+                Obj_BD_DAL.Obj_sql_adap.Fill(DS);
+                // Se retorna el DataTable de índice 0 que está en el DataSet
+                return DS.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                sMsjError = e.ToString().Trim();
                 return null;
             }
             finally
---
+++ ClubCampestre/WCF/Interfaces/IMantenimientosGenericos.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.ServiceModel;
+
+namespace WCF.Interfaces
+{
+    [ServiceContract]
+    public interface IMantenimientosGenericos
+    {
+        [OperationContract]
+        DataTable ListarDatos(string sNombreSP, ref string sMsjError);
+
+        [OperationContract]
+        DataTable FiltrarDatos(string sNombreSP, string sNombreParametro, SqlDbType DbType, string sValorParametro, ref string sMsjError);
+
+        [OperationContract]
+        bool Insertar_DatosSinIdentity(string sNombreSP, DataTable dtParametros, ref string sMsjError);
+
+        [OperationContract]
+        string Insertar_DatosConIdentity(string sNombreSP, DataTable dtParametros, ref string sValorScalar, ref string sMsjError);
+
+        [OperationContract]
+        bool Modifica_Datos(string sNombreSP, DataTable dtParametros, ref string sMsjError);
+
+        [OperationContract]
+        bool Elimina_Datos(string sNombreSP, DataTable dtParametros, ref string sMsjError);
+    }
+}
