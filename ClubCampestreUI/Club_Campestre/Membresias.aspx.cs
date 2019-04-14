dbe85e9fbd19fadc5559d9fffd2eaa650d9c5e6d
Marco Gomez Guzman <mgomez@Aseccss.com>
2019-04-14 15:21:23 -0600

    membresias

--- ClubCampestreUI/Club_Campestre/Membresias.aspx.cs
+++ ClubCampestreUI/Club_Campestre/Membresias.aspx.cs
@@ -9,78 +9,78 @@ using ClubCampestre_BLL.CatalogosMantenimientos;
 
 namespace Club_Campestre
 {
-    public partial class Membresias : System.Web.UI.Page
-    {
+    //    public partial class Membresias : System.Web.UI.Page
+    //    {
 
-        #region Variables Globales
-
-
-         Obj_Estado_BLL = new Cls_Estado_BLL();
-        Cls
-        Cls_Estado_DAL Obj_Estado_DAL;
-        #endregion
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-                this.BindGrid();
-            }
-        }
-
-        private void BindGrid()
-        {
-            //Se instancia objeto
-            Obj_Estado_DAL = new Cls_Estado_DAL();
-
-            if (this.txtFiltraEstados.Text == string.Empty)//listar
-            {
-                //llamado metodo listar estados
-                Obj_Estado_BLL.Listar(ref Obj_Estado_DAL);
-
-            }
-            else
-            {
-                Obj_Estado_DAL.SEstado = this.txtFiltraEstados.Text;
-                //llamado metodo listar estados
-                Obj_Estado_BLL.Filtrar(ref Obj_Estado_DAL);
-            }
-
-            if (Obj_Estado_DAL.sMsjError == string.Empty)
-            {
-                //Carga de Grid con DataSet instanciado en DAL
-                this.EstadoGridView.DataSource = Obj_Estado_DAL.DS.Tables[0];
-                this.EstadoGridView.DataBind();
-            }
-            else
-            {
-                this.errorMensaje.InnerHtml = "Se presento un error a la hora de listar Estados.";
-                this.BindGrid();
-            }
+    //        #region Variables Globales
 
 
-        }
+    //        // Obj_Estado_BLL = new Cls_Estado_BLL();
+    //        //Cls
+    //        //Cls_Estado_DAL Obj_Estado_DAL;
+    //        //#endregion
 
-        protected void btnEliminar_Click(object sender, EventArgs e)
-        {
+    //        protected void Page_Load(object sender, EventArgs e)
+    //        {
+    //            if (!IsPostBack)
+    //            {
+    //                this.BindGrid();
+    //            }
+    //        }
 
-        }
+    //        private void BindGrid()
+    //        {
+    //            //Se instancia objeto
+    //            //Obj_Estado_DAL = new Cls_Estado_DAL();
 
-        protected void btnEditar_Click(object sender, EventArgs e)
-        {
+    //            //if (this.txtFiltraEstados.Text == string.Empty)//listar
+    //            //{
+    //            //    //llamado metodo listar estados
+    //            //    Obj_Estado_BLL.Listar(ref Obj_Estado_DAL);
 
-        }
+    //            //}
+    //            //else
+    //            //{
+    //            //    Obj_Estado_DAL.SEstado = this.txtFiltraEstados.Text;
+    //            //    //llamado metodo listar estados
+    //            //    Obj_Estado_BLL.Filtrar(ref Obj_Estado_DAL);
+    //            //}
 
-        protected void btnNuevo_Click(object sender, EventArgs e)
-        {
+    //            //if (Obj_Estado_DAL.sMsjError == string.Empty)
+    //            //{
+    //            //    //Carga de Grid con DataSet instanciado en DAL
+    //            //    this.EstadoGridView.DataSource = Obj_Estado_DAL.DS.Tables[0];
+    //            //    this.EstadoGridView.DataBind();
+    //            //}
+    //            //else
+    //            //{
+    //            //    this.errorMensaje.InnerHtml = "Se presento un error a la hora de listar Estados.";
+    //            //    this.BindGrid();
+    //            //}
 
-        }
 
-        protected void btnBuscar_Click(object sender, EventArgs e)
-        {
+    //        }
 
-        }
+    //        protected void btnEliminar_Click(object sender, EventArgs e)
+    //        {
+
+    //        }
+
+    //        protected void btnEditar_Click(object sender, EventArgs e)
+    //        {
+
+    //        }
+
+    //        protected void btnNuevo_Click(object sender, EventArgs e)
+    //        {
+
+    //        }
+
+    //        protected void btnBuscar_Click(object sender, EventArgs e)
+    //        {
+
+    //        }
 
 
-    }
+    //    }
 }
