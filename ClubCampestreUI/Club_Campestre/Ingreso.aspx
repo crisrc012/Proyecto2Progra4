58570514b5eaa7cd2c4b03b8570bfe88715ca262
milton.villalobos <milton.villalobos@QS44>
2019-04-19 09:15:40 -0600

    formulario ingresos

---
+++ ClubCampestreUI/Club_Campestre/Ingreso.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Ingreso.aspx.cs" Inherits="Club_Campestre.Ingreso" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="mainContent" runat="server">
+          <div class ="main" style="width: 86%; margin-left: 0px; margin-right: 150px;">
+        <div class="pure-control-group">
+        <div >
+        <header>
+            <h1 >Ingresos</h1>
+        </header>
+    </div>
+    <div>
+         <form class = "pure-form pure-form-aligned" method="post">
+          <fieldset>
+           <div class="pure-control-group">
+             
+            <label for="cedula"> Cedula: </label>
+            <input runat="server" type="text" id ="txtCedula" value="" style="color: #0090ff"/>
+              <label for="Rol"> TipoCliente: </label>
+              <input runat="server" type="text" id="TxtTipoCliente"  ForeColor="Blue" Width="191px" />
+          </div>
+
+          <div class="pure-control-group">
+            <label for="nombre"> Nombre: </label>
+              <input runat="server" type="text" id="txtnombre" onkeypress="javascript:return soloLetras(event)" style="color: #0090ff" />
+         <div > </div>
+              <div class="pure-control-group">
+             <label for="cedula"> Membresia: </label>
+            <input runat="server" type="text" id ="TxtMembresia" value="" style="color: #0090ff"/>
+              <label for="Rol"> Costo: </label>
+              <input runat="server" type="text" id="TxtCosto"  ForeColor="Blue" Width="191px" />
+             </div>
+               </div>
+               <label for="telefono" > Invitado</label><input runat="server" type="text" id="txtInvitado"  style="color: #0090ff; width: 284px;" />
+              <asp:Button class="pure-button pure-button-primary" ID="btnagregarInvitado" runat="server" Text="+"   />
+              &nbsp;
+              <asp:Button  class="pure-button pure-button-primary"  ID="btnremoverInvitado" runat="server" Text="-"  />
+              <div class="pure-controls">
+                    <asp:GridView class="pure-table" ID="GridViewIvitados" runat="server" AutoGenerateColumns="false" ForeColor="Blue"  Height="156px" Width="400px" >
+                <Columns>
+                    <asp:BoundField DataField="IdPersona" HeaderText="Cedula" />
+                    <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+                    <asp:BoundField DataField="Tipo" HeaderText="Tipo" />
+                    <asp:BoundField DataField="Costo" HeaderText="Costo" />
+                    
+                </Columns>
+            </asp:GridView>
+            <br />
+        </div>
+
+               <label for="correo"> Servicios: </label>
+                    <div class="pure-controls">
+                    <asp:GridView class="pure-table" ID="CorreoPersonaGridView" runat="server"  Height="156px" Width="400px" AutoGenerateColumns="false" ForeColor="Blue" OnSelectedIndexChanged="CorreoPersonaGridView_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="IdTipoServicio" HeaderText="IdTipoServicio" />
+                    <asp:BoundField DataField="Descripcion" HeaderText="Servicio" />
+                    <asp:BoundField DataField="costo" HeaderText="Costos" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:CheckBox ID="chkRow" runat="server" ItemStyle-Width="5" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+        </div>
+        <div class="pure-controls" style="width:  330px; margin:0 auto; ">
+            <input class="pure-button pure-button-primary"  id="btnRegresar" type="button" value="Regresar" onclick="location.href = 'Mant_Persona.aspx' "  /> 
+             &nbsp;  
+            <asp:Button class="pure-button pure-button-primary" ID="btnGuardar" runat="server" Text="Guardar"  OnClick="btnGuardar_Click1" />
+           </div>
+
+          </fieldset>
+        </form>
+             </div>
+    </div>
+           </div>
+</asp:Content>
---
+++ ClubCampestreUI/Club_Campestre/Ingreso.aspx.designer.cs
@@ -0,0 +1,114 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Club_Campestre {
+    
+    
+    public partial class Ingreso {
+        
+        /// <summary>
+        /// Control txtCedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtCedula;
+        
+        /// <summary>
+        /// Control TxtTipoCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText TxtTipoCliente;
+        
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtnombre;
+        
+        /// <summary>
+        /// Control TxtMembresia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText TxtMembresia;
+        
+        /// <summary>
+        /// Control TxtCosto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText TxtCosto;
+        
+        /// <summary>
+        /// Control txtInvitado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtInvitado;
+        
+        /// <summary>
+        /// Control btnagregarInvitado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnagregarInvitado;
+        
+        /// <summary>
+        /// Control btnremoverInvitado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnremoverInvitado;
+        
+        /// <summary>
+        /// Control GridViewIvitados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridViewIvitados;
+        
+        /// <summary>
+        /// Control CorreoPersonaGridView.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView CorreoPersonaGridView;
+        
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
