5d964c2d3f20bb61898a1eb9321f97e00077d01b
milton.villalobos <milton.villalobos@QS44>
2019-04-20 23:59:54 -0600

    prueba

--- ClubCampestreUI/Club_Campestre/Ingreso.aspx.cs
+++ ClubCampestreUI/Club_Campestre/Ingreso.aspx.cs
@@ -21,7 +21,7 @@ namespace Club_Campestre
         
         #endregion
         protected void Page_Load(object sender, EventArgs e)
-        {
+        { 
             if (!IsPostBack)
             {
                 this.BindGrid();
